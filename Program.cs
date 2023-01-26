1ddc8dcd58a92a770684a253ba1778f8eafd1412
Ved_s <53968411+Ved-s@users.noreply.github.com>
2023-01-27 02:29:23 +1100

    bring crash message back

--- Program.cs
+++ Program.cs
@@ -2,20 +2,20 @@
 using System.Diagnostics;
 using System.Windows.Forms;
 
-//try
-//{
+try
+{
     using var game = new RainMap.Main();
     game.Run();
-//}
-//catch (Exception ex)
-//{
+}
+catch (Exception ex)
+{
 //    if (Debugger.IsAttached)
 //        throw;
-//
-//    MessageBox.Show(
-//$@"Caught fatal exception: 
-//
-//{ex.GetType().Name}: {ex.Message}
-//{ex.StackTrace}
-//", "RainMap has crashed");
-//}
+
+    MessageBox.Show(
+$@"Caught fatal exception: 
+
+{ex.GetType().Name}: {ex.Message}
+{ex.StackTrace}
+", "RainMap has crashed");
+}
